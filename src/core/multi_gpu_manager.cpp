150199bcedf9fbb1473dcd3fa27ad934dc4c9beb
Frenchchocolatine <159849274+frenchchoco@users.noreply.github.com>
2025-08-06 23:45:17 +0200

    Update multi_gpu_manager.cpp

--- src/core/multi_gpu_manager.cpp
+++ src/core/multi_gpu_manager.cpp
@@ -1,4 +1,4 @@
-#include "include/multi_gpu_manager.hpp"
+#include "core/multi_gpu_manager.hpp"
 #include "logging/logger.hpp"
 #include "core/gpu_api.h"
 #include "include/miner/kernel_launcher.hpp"
